5fe388695f54e412bffb8a81524ca764a1da92b1
Brian Campbell <campbellsoupbs@gmail.com>
2022-05-26 21:58:26 -0400

    Complete sumAll exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
+const sumAll = function(start, stop) {
+    let sum = 0;
+    if (typeof(start) !== 'number' || typeof(stop) !== 'number') 
+        {return 'ERROR'}
+    else if (start < 0 || stop < 0)
+        {return 'ERROR'}
+    else if (start < stop) {
+        for (i = start; i <= stop; i++) {sum += i}} 
+    else if (start > stop) {
+        for (i = stop; i <= start; i++) {sum += i}}
 
+
+    return sum;
 };
 
 // Do not edit below this line
