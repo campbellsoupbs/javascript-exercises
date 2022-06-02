3d858a098f3dc739338a5a4ae8ecd5e6efa0288e
Brian Campbell <campbellsoupbs@gmail.com>
2022-06-02 19:54:58 -0400

    Finished palindromes exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,21 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    let forward = '';
+    let backward = '';
+    
+    for (let x of string) {
+        if (x.match(/[a-zA-Z]/) !== null) {
+            forward = forward+x;
+            backward = x+backward;
+        }
+    }
+    
+    forward = forward.toLowerCase();
+    backward = backward.toLowerCase();
 
+    return forward === backward
+
+    // console.log(`foward: ${forward}`);
+    // console.log(`backward: ${backward}`);
 };
 
 // Do not edit below this line
