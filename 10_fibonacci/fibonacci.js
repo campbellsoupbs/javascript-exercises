b3f3001ab07d84249eaa5e9d9bbbf6b3f6c0b515
Brian Campbell <campbellsoupbs@gmail.com>
2022-06-02 20:40:41 -0400

    Finished fibonacci exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,14 @@
-const fibonacci = function() {
+const fibonacci = function(index) {
+    let fib = [1,1];
+    if (index < 0) {
+        return "OOPS";
+    } else {
+        for (i=2; i <= index-1; i++) {
+            let newnum = fib[i-1]+fib[i-2];
+            fib.push(newnum);
+        }
+        return fib[(index-1)];
+    }
 
 };
 
