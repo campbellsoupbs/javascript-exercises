974fefbed4410fab4af9e4a91a46708d115611b2
Brian Campbell <campbellsoupbs@gmail.com>
2022-06-03 00:42:57 -0400

    Finished findTheOldest exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    people.forEach(person => {
+        if ("yearOfDeath" in person) {
+            person['age'] = person.yearOfDeath - person.yearOfBirth;
+        } else {
+            person['age'] = (new Date()).getFullYear() - person.yearOfBirth;
+        }
+    });
 
+    people.sort((a,b) => (a.age > b.age) ? -1:1);
+
+    // console.table(people);
+    return people[0];
 };
 
 // Do not edit below this line
