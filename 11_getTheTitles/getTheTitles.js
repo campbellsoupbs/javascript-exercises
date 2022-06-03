bd72eb5ef9ee51546d6e64a3fad60f6f7b63ba6f
Brian Campbell <campbellsoupbs@gmail.com>
2022-06-02 20:52:46 -0400

    Finish getTheTitles exercise

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(array) {
+    let answer = [];
+    array.forEach(element => {
+        answer.push(element['title']);
+    });
+    return answer;
 };
 
 // Do not edit below this line
